eb38cb1a3420db65dc06529f8d0da4bfd432533c
renaldyhidayatt <renaldyhidayatt@gmail.com>
2022-11-26 20:08:41 +0700

    Adding

---
+++ src/main/java/com/sanedge/newspaginate/NewspaginateApplication.java
@@ -0,0 +1,13 @@
+package com.sanedge.newspaginate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewspaginateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewspaginateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanedge/newspaginate/NewspaginateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanedge.newspaginate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewspaginateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
